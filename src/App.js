d9e7ec3893a9c5783f77054107ce30964eae67a4
Andrew Horn <arhorn@smcm.edu>
2020-05-12 18:07:05 -0500

    ran prettier so that formatting is easier to read

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import SideBar from './components/sidebar/SideBar';
-import Content from './components/content/Content';
-import './App.css';
+import SideBar from "./components/sidebar/SideBar";
+import Content from "./components/content/Content";
+import "./App.css";
 
 const App = () => {
-
-  const [sidebarIsOpen, setSidebarOpen] = useState(true)
-  const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen)
+  const [sidebarIsOpen, setSidebarOpen] = useState(true);
+  const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
 
   return (
     <Router>
       <div className="App wrapper">
-        <SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen}/>
-        <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen}/>
+        <SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
+        <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
       </div>
     </Router>
   );
-}
+};
 
-export default App
+export default App;
